0617840eb5bacd27237e3738fd93ba811c0bdce8
shubhamjangid-dev <s.jangid1100@gmail.com>
2024-04-20 21:46:02 +0530

    connected to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("MONGODB Connected Successfully");
+    } catch (error) {
+        console.log("MONGODB Connection FAILED",error);
+        process.exit(1) // app band kr ne ke liye
+    }
+}
+
+export default connectDB; // ye jaruri h
---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config() // proper tarika niche h
+
+import dotenv from "dotenv";
+// import mongoose from "mongoose";             // ye dono db folder me transfer kr diya
+// import { DB_NAME } from "./constants";       // database connection db folder me hi hoga
+import express from "express";
+import connectDB from "./db/index.js"
+const app = express();
+dotenv.config({
+    path :'./.env'
+}) // ye require walw me nahi krna
+
+connectDB();
+// ek bbat ratlo database dusre continent me h to time lgta
+// error bhi bahut aati h
+// to hamesha try catch or async await ka use krna
+
+// IIFE 
+// ( async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (error)=>{
+//             console.log("ERRORR",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("ERROR",error);
+//     }
+// })()
